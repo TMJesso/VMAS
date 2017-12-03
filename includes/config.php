ac00babc410e45705a5b24845431d1ae04a0b919
Theral Jessop <Jess_Hort_Farms@mail.com>
2017-12-03 15:56:09 -0500

    Changed config

--- includes/config.php
+++ includes/config.php
@@ -5,11 +5,11 @@
 // https://wiki.filezilla-project.org/Network_Configuration
 
 // database constants
-defined('DB_SERVER')	? null : define("DB_SERVER", "localhost");
-defined('DB_USER')		? null : define("DB_USER", "afr10w");
-defined('DB_PASS')		? null : define("DB_PASS", "p6iFQRnx8B976CQv");
-defined('DB_NAME')		? null : define("DB_NAME", "vmas");
-defined('DB_PORT') 		? null : define("DB_PORT", 3306);
+defined('DB_SERVER')	? null : define('DB_SERVER', 'localhost');
+defined('DB_USER')		? null : define('DB_USER', 'aFR10w');
+defined('DB_PASS')		? null : define('DB_PASS', 'p6iFQRnx8B976CQv');
+defined('DB_NAME')		? null : define('DB_NAME', 'vmas');
+defined('DB_PORT') 		? null : define('DB_PORT', 3306);
 
 
 ?>
